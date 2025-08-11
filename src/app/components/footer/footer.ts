2b358b3863ffd2fbb365d18282d4013365584a71
Adrian Gaborek <A.Gaborek@devnet.de>
2025-08-11 20:35:04 +0200

    implement expandable footer section with smooth toggle animation

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RichTextPipe } from '../../pipes/rich-text-pipe';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [RichTextPipe],
   templateUrl: './footer.html'
 })
-export class Footer {
+export class Footer implements AfterViewInit {
   @Input() blok: any;
+  @ViewChild('content') contentEl!: ElementRef<HTMLDivElement>;
+
+  expanded = false;
+  contentHeight = 0;
+
+  ngAfterViewInit() {
+    this.contentHeight = this.contentEl.nativeElement.scrollHeight;
+  }
+
+  toggle() {
+    const el = this.contentEl.nativeElement;
+    const fullHeight = el.scrollHeight;
+
+    if (!this.expanded) {
+      gsap.to(el, {
+        height: fullHeight,
+        duration: 0.4,
+        ease: 'power2.out',
+        onComplete: () => {
+          el.style.height = 'auto';
+        }
+      });
+    } else {
+      const lineHeight = parseFloat(getComputedStyle(el).lineHeight) || 16;
+      const clampHeight = lineHeight * 2;
+
+      gsap.to(el, {
+        height: clampHeight,
+        duration: 0.3,
+        ease: 'power2.inOut'
+      });
+    }
+
+    this.expanded = !this.expanded;
+  }
 }
